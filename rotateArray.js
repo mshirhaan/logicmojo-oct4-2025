7d405bd906b7740df95a009201dac6de8c006f19
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2025-10-25 11:35:09 +0530

    Add complexity annotations for rotate function
    Add time and space complexity comments for rotate function

---
+++ rotateArray.js
@@ -0,0 +1,23 @@
+//TC: O(n)
+//SC: O(n)
+var rotate = function(nums, k) {
+    let n = nums.length;
+    if(n == 1) return  
+
+    k = k % n;
+
+    reverse(nums, 0, n-1)
+    reverse(nums, 0, k-1)
+    reverse(nums,k, n-1)
+};
+
+function reverse(nums, start, end) {
+    while(start<end) {
+        let temp = nums[start]
+        nums[start] = nums[end]
+        nums[end] = temp
+
+        start++;
+        end--;
+    }
+}
