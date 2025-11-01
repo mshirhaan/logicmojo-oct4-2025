f1047c33649a4286100c844323529628f141b924
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2025-11-01 12:26:38 +0530

    Add solution: Majority Element II

--- solutions/Array/15-majority-element-ii.js
+++ solutions/Array/15-majority-element-ii.js
@@ -2,27 +2,27 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var majorityElement = function(nums) {
+var majorityElement = function (nums) {
     let seat1 = null, seat2 = null, countSeat1 = 0, countSeat2 = 0
 
-    for(let num of nums) {
-        if(seat1 == num) {
+    for (let num of nums) {
+        if (seat1 == num) {
             countSeat1++
         } else if (seat2 == num) {
             countSeat2++
         }
-        else if(seat1 == null) {
+        else if (seat1 == null) {
             seat1 = num
-        } else if(seat2 == null) {
+        } else if (seat2 == null) {
             seat2 = num
         } else {
-            if(countSeat1 > 0) {
+            if (countSeat1 > 0) {
                 countSeat1--
             } else {
                 seat1 = null
             }
 
-            if(countSeat2 > 0) {
+            if (countSeat2 > 0) {
                 countSeat2--
             } else {
                 seat2 = null
@@ -31,23 +31,21 @@ var majorityElement = function(nums) {
     }
 
     let op = []
-    if(seat1 !=null) {
-        let tempCount = 0;
-        for(let num of nums) {
-            if(seat1 == num)
-                tempCount++
-        }
-        if(tempCount>nums.length/3)
-            op.push(seat1)
+
+    let tempCount1 = 0;
+    let tempCount2 = 0;
+    for (let num of nums) {
+        if (seat1 == num)
+            tempCount1++
+        if (seat2 == num)
+            tempCount2++
     }
-    if(seat2 !=null) {
-        let tempCount = 0;
-        for(let num of nums) {
-            if(seat2 == num)
-                tempCount++
-        }
-        if(tempCount>nums.length/3)
-            op.push(seat2)
-    }
+
+    if (tempCount1 > nums.length / 3)
+        op.push(seat1)
+
+    if (tempCount2 > nums.length / 3)
+        op.push(seat2)
+
     return op;
 };
