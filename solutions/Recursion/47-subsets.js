ca57106895fe5bfd53b64ddade2d9c236322d590
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2025-11-22 12:20:31 +0530

    Add solution: Subsets

---
+++ solutions/Recursion/47-subsets.js
@@ -0,0 +1,16 @@
+function subsets(nums) {
+  let trolly = []
+  helper(0, [], trolly);
+  return trolly;
+
+  function helper(i, bag, trolly) {
+    if(i == nums.length) {
+      trolly.push([...bag]);
+      return
+    }
+    bag.push(nums[i])
+    helper(i+1, bag, trolly);
+    bag.pop()
+    helper(i+1, bag, trolly);
+  }
+}
